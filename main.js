a86b62ad62d3b1b1f813591ecd19336ca4aa59d9
Gabriel Archieve <114693768+Gabriel-Dupla@users.noreply.github.com>
2023-03-08 13:36:38 -0300

    Add files via upload
    Adicionando os arquivo

---
+++ main.js
@@ -0,0 +1,65 @@
+function tocaSom(seletorAudio) { 
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+
+    }
+ 
+}
+let contadorDeEnter = 0;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let i=0; i <listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    // template string
+    const idAudio = `#som_${instrumento}`
+
+    // console.log(instrumento);
+    listaDeTeclas[i].onclick = function () {
+        tocaSom(idAudio)
+    };
+
+    tecla.onkeydown = function (event) {
+        console.log(event)
+        if (event.code === "Space" || event.code === "Enter") {
+            if(event.code === 'Enter') {
+                contadorDeEnter++;
+                if (contadorDeEnter >= 2) { 
+                    event.preventDefault(); 
+               }
+             }
+             tecla.classList.add('ativa');
+        }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+        contadorDeEnter = 0;
+    }
+
+    }
+}
+    //     if(event.code === 'Enter' || event.code === 'NumpadEnter') {
+    //         if(event.code === 'Enter') {
+    //             contadorDeEnter++;
+    //             if (contadorDeEnter >= 2) { 
+    //                event.preventDefault(); 
+    //             }
+    //         }
+    //         tecla.classList.add('ativa');
+    //     }
+
+    // }
+
+    // tecla.onkeyup = function () {
+	// 	tecla.classList.remove('ativa');
+    //     contadorDeEnter = 0;
+    // }
+
+
+//}
